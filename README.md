f691c4b249aace3705b9795f14d097c88f4280d5
刘泽 <liuze03@meituan.com>
2016-02-23 11:48:28 +0800

    add isEqual method

--- README.md
+++ README.md
@@ -20,6 +20,8 @@ path //array which define a scope for the changing part,the rest will be shared
 value //if path exist alter the path value  else create the path then define the value
 */
 var newObj = update(obj,path,value);
+
+update.isEqual(obj,newObj);//check if value is equal not reference,meant for react rerender check
 ```
 
 #usage
@@ -36,7 +38,7 @@ var a = {
     },
     c: {
         x: [1,2,3],
-        y: [1,2,3,4]
+        y: [1,{name'liu'},3,4]
     }
 };
 
@@ -48,12 +50,8 @@ var c = update(a, 'province.address.zone', 'C');
 
 console.log('a',a,'\n' ,'c',c,'\n');
 
-var d = update(a, ['c','y',1], 10);
+var d = update(a, ['c','y',1], {name:'liu'});
 
-console.log('a',a,'\n' ,'b',d,'\n', a.c.x == d.c.x , a.c.y , d.c.y);
-
-var e = update(a, ['c','d'], 2);
-
-console.log('a',a,'\n' ,'b',e,'\n', a.d , e.d);
+console.log(a,d,a==d,isEqual(a,d));//false  true
 
 ```
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+var isObject = function(o){
+	return typeof o == 'object';
+};
 
 var update= function(obj,path,value){
 	
 	if(typeof path === 'string') path = path.split('.');//scope string args
+
+	path = path.slice(0);//create clone to avod modify argument path
 
 	var func = function(obj,path){
 
@@ -11,7 +16,7 @@ var update= function(obj,path,value){
 
 		var retObj = ( (typeof item == 'string') ? {} : []);
 
-		if(typeof obj == 'object'){
+		if(isObject(obj)){
 
 			for(var key in obj){
 				if(key !== item){
@@ -21,13 +26,47 @@ var update= function(obj,path,value){
 
 		}
 
-	    retObj[item] = func( obj&&obj[item] || 'undefined',path)
+	    retObj[item] = func( obj&&obj[item] || 'undefined',path);
 
 	    return retObj;
 
 	}
 
 	 return func(obj,path);
-}
+};
+
+var isEqual = function(obj1,obj2){
+
+	var key;
+
+	if(isObject(obj1) && isObject(obj2)){
+
+		for(key in obj1){
+
+			if(obj2.hasOwnProperty(key)){
+
+				if(!isEqual(obj1[key],obj2[key]) ){
+					return false;
+				}
+
+			}else{
+				return false;
+			}
+		}
+
+	}else if(obj1 == obj2){
+
+		return true;
+
+	}else{
+
+		return false;
+
+	}
+
+	return true;
+};
+
+update.isEqual = isEqual;
 
 module.exports = update;
